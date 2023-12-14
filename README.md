52fd90254e5085b14e3fe4d4373befdbbe8f1575
meghajibhakate <meghajibhakate18@gmail.com>
2023-12-14 19:12:39 +0530

    html, css

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Aikyam-space-Template
+# bastar-se-bazaar
+Website for Bastar Se Bazaar Tak
+
+## Development
+
+`git clone https://github.com/t4glabs/bastar-se-bazaar`
+
+`cd bastar-se-bazaar`
+
+`npm install`
+
+## Build CSS
+
+`npm run build-css`
+
+After running the build-css command, edit/create html files in the root folder.
---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+
+	
+
+let contents1=document.getElementById("box1");
+let contents2=document.getElementById("box2");
+let btn=document.getElementById("btn");
+
+function contents_box(){
+    contents1.style.display='block'
+    contents2.style.display='block'
+    btn.style.display='none'
+}
+
+let testimonials_1=document.getElementById("testimonial-1");
+let testimonials_2=document.getElementById("testimonial-2");
+let testimonials_3=document.getElementById("testimonial-3");
+let testimonials_btn=document.getElementById("testimonials_btn");
+// let contents2=document.getElementById("box2");
+
+
+
+function testimonial(){
+    testimonials_1.style.display='block'
+    testimonials_2.style.display='block'
+    testimonials_3.style.display='block'
+    testimonials_btn.style.display='none'
+}
